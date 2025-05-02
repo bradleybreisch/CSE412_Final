3cdfd0c024e69697722ad0fac9bb83f9bc48f261
Bradley Breisch <bradleybreisch@gmail.com>
2025-05-01 20:21:24 -0700

    search bar

---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+ 
+ const SearchBar = ({ onSearch }) => {
+     const [query, setQuery] = useState("");
+ 
+     const handleSearch = async (e) => {
+         e.preventDefault();
+         onSearch(query);
+     };
+ 
+     return (
+         <form onSubmit={handleSearch}>
+             <input
+                 value={query}
+                 onChange={(e) => setQuery(e.target.value)}
+                 placeholder="Search by title"
+             />
+             <button type="submit">Search</button>
+         </form>
+     );
+ };
+
+ export default SearchBar;
